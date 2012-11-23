9b3afc194615e0cb792a110e6de53868ba7e4689
ykurdijaka <ykurdijaka>
2012-11-23 11:16:56 +0100

    Refactor calcul montant

--- src/main/java/com/gestimmo/metier/model/Bien.java
+++ src/main/java/com/gestimmo/metier/model/Bien.java
@@ -6,8 +6,6 @@ import org.joda.time.Days;
 import org.joda.time.Interval;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Bien {
 	//	Chateau,
@@ -129,24 +127,18 @@ public class Bien {
 
 	public double calculerPrixLocation(DateTime dateDebutReservation, DateTime dateFinReservation) {
 		double prixARetourner = 0;
-		HashMap<Integer, Periode> listePeriodes = new HashMap<Integer, Periode>();
+		int totalJour;
+
+		Interval intervalReservation = new Interval(dateDebutReservation, dateFinReservation);
+		Interval intervalEntrePeriode, intervalDates;
+
 		for (Periode p : periodes) {
+			intervalDates = new Interval(p.getDateDebut(), p.getDateFin());
 
-			Interval intervalDates = new Interval(p.getDateDebut(), p.getDateFin());
-
-			if (intervalDates.contains(dateDebutReservation) && intervalDates.contains(dateFinReservation)) {
-				listePeriodes.put(Days.daysBetween(dateDebutReservation.toDateMidnight(), dateFinReservation.toDateMidnight()).getDays(), p);
-			} else if (intervalDates.contains(dateDebutReservation)) {
-				listePeriodes.put((Days.daysBetween(dateDebutReservation.toDateMidnight(), p.getDateFin().toDateMidnight()).getDays()), p);
-			} else if (intervalDates.contains(dateFinReservation)) {
-				listePeriodes.put((Days.daysBetween(p.getDateDebut().toDateMidnight(), dateFinReservation.toDateMidnight()).getDays()), p);
-			} else if (!intervalDates.isBefore(dateDebutReservation) && !intervalDates.isAfter(dateFinReservation)) {
-				listePeriodes.put((Days.daysBetween(p.getDateDebut().toDateMidnight(), p.getDateFin().toDateMidnight()).getDays()), p);
+			if ( ( intervalEntrePeriode = intervalDates.overlap(intervalReservation) ) != null) {
+				totalJour = Days.daysBetween(intervalEntrePeriode.getStart().toDateMidnight(), intervalEntrePeriode.getEnd().toDateMidnight()).getDays();
+				prixARetourner += totalJour * p.getMontant();
 			}
-		}
-
-		for (Map.Entry<Integer, Periode> e : listePeriodes.entrySet()) {
-			prixARetourner += e.getKey() * e.getValue().getMontant();
 		}
 
 		return prixARetourner;
